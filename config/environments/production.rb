9f4a75637aab71eb825fb331249ecf4302eec27a
hillman <ayuktataw009@gmail.com>
2021-04-24 18:14:49 +0100

    changed the senders email to tatawhillman

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
    #:domain => 'myapp.herokuapp.com',  #I've also tried changing this to 'gmail.com'
    :authentication => :plain, # I've also tried changing this to :login
    :enable_starttls_auto => true,
-   :user_name => 'chriskeller619@gmail.com',
-   :password => '@Leroyfer2'
+   :user_name => 'tatawhillman@gmail.com',
+   :password => 'poissonous'
  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
