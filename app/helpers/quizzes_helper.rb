94d69ced013dd8ed6226f2c9172db29a4ecac3ad
hillman <ayuktataw009@gmail.com>
2021-05-22 19:53:09 +0100

    added statistics to quiz results

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -5,4 +5,21 @@ module QuizzesHelper
 	def quiz_number(les_id)
 		UserLessonQuiz.where(lesson_id: current_lesson.id, completed: true , user_id: current_user.id).count + 1
 	end
+	def get_failed
+		UserLessonQuiz.where(user_id: current_user.id,lesson_id: current_lesson.id, verdict: "failed").count
+	end
+	def get_passed
+		UserLessonQuiz.where(user_id: current_user.id, lesson_id: current_lesson.id, verdict: "passed").count
+	end
+
+	def get_percentage
+		(get_passed.to_f/Lesson.find(current_lesson.id).quizzes.count.to_f)*100
+	end
+	def get_decision
+		if get_percentage < 60
+			return "Not completed"
+		else
+			return "Completed"
+		end
+	end
 end
--- app/models/user_lesson_quiz.rb
+++ app/models/user_lesson_quiz.rb
@@ -7,9 +7,9 @@ class UserLessonQuiz < ApplicationRecord
 		UserLessonQuiz.where(quiz_id: q_id)
 	end
 	def self.purge(l_id)
-		records = UserLessonQuiz.where(lesson_id: l_id)
-		records.each do |rec|
-			rec.destroy
-		end
+		records = UserLessonQuiz.where(lesson_id: l_id).delete_all
 	end
+
+	scope :failed, ->(u_id, l_id){where("user_id: ?, lesson_id: ?,verdict: ?", u_id, l_id, "failed")}
+	scope :passed, ->(u_id, l_id){where("user_id: ?, lessson_id: ?,verdict: ?", u_id, l_id, "passed")}
 end
