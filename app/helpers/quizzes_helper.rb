e9eacba919d851dc21f9c507030bfd9a18807e62
hillman <ayuktataw009@gmail.com>
2021-05-22 18:24:29 +0100

    style changes to the back button

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,8 +1,8 @@
 module QuizzesHelper
   def finished_quizzes?(les_id)
-    UserLessonQuiz.where(lesson_id: les_id, completed: true).count == Lesson.find(les_id).quizzes.count
+    UserLessonQuiz.where(lesson_id: les_id, completed: true, user_id: current_user.id).count == Lesson.find(les_id).quizzes.count
   end
 	def quiz_number(les_id)
-		finished_quizzes(les_id) + 1
+		UserLessonQuiz.where(lesson_id: current_lesson.id, completed: true , user_id: current_user.id).count + 1
 	end
 end
